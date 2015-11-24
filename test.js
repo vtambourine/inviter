1989e291042e6b73de78ecd850bb3c5430d21107
Benjamin Tambourine <vtambourine@ya.ru>
2015-11-24 21:13:51 +0300

    Draft

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-require('babel-polyfill');
+var GoogleSpreadsheet = require("google-spreadsheet");
+// var googleApiInfo = require('3a6885fba6e7.json');
 
-function getValues() {
-  return Promise.resolve([1,2,3,4]);
-}
 
-async function foo() {
-  var values = await getValues();
-  console.log(values);
-}
+var my_sheet = new GoogleSpreadsheet('1DEv084v7VYX3kzBs5dZPAYoE9omGCZh-zY4wQQv23-Y');
+
+my_sheet.getRows( 1, function(err, row_data){
+    console.log( 'pulled in '+row_data.length + ' rows');
+});
+
+my_sheet.useServiceAccountAuth(require('./3a6885fba6e7.json'));
